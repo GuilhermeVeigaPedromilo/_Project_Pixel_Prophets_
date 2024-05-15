b2962b04d89232f2a7e871ad22174892304a341c
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-05-15 09:10:30 -0300

    update

--- src/server/API.js
+++ src/server/API.js
@@ -4,7 +4,6 @@ const cors = require("cors"); // Importe o pacote cors
 const mysql = require('mysql');
 const session = require('express-session');
 const path = require('path');
-const { NEWDATE } = require('mysql/lib/protocol/constants/types');
 
 const app = express();
 
@@ -38,27 +37,28 @@ const db = mysql.createConnection({
   );
 
   app.post('/Cadastro', (req, res) => {
-    const { nome, cpf, email, senha, } = req.body
-    console.log(`${JSON.stringify(body)}`);
+    const { nome, cpf, email, senha, Saldo, numConta} = req.body
+    
+    console.log(`${JSON.stringify(req.body)}`);
 
-    console.log(`${nome} ${cpf} ${email} ${senha}`);
+    // console.log(`Nome:${nome}, CPF:${cpf}, E-mail:${email}, Senha:${senha}`);
 
-    const verification = 'SELECT * FROM users WHERE username = ? OR cpf = ? OR numConta = ?';
+    const verification = 'SELECT * FROM users WHERE nome = ? OR cpf = ?';
     console.log(`${verification}`);
     
-    db.query(verification, [nome, cpf, numConta], (verifErr, results) => {
+    db.query(verification, [nome, cpf ], (verifErr, results) => {
       if (verifErr) {
-        console.error('Erro no inserimento dos dados', checkErr);
+        console.error('Erro no inserimento dos dados', verifErr);
       }
+      console.log(`${results}`);
       if (results.length <= 0) {
-        console.log(`Inserindo novo usuário no sistema com estes dados: ${username} - ${cpf}`);
-        const query = 'INSERT INTO users (nome, cpf, email, senha, numConta, Saldo, tipo) VALUES (?, ?, ?, SHA1(?), ?, "123456789", "1000", "user")';
+        console.log(`Inserindo novo usuário no sistema com estes dados: ${nome} - ${cpf}`);
+        const query = `INSERT INTO users (nome, cpf, email, senha, numConta, Saldo, tipo) VALUES (?, ?, ?, SHA1(?), '${cpf}', "1000", "user")`;
         db.query(query, [nome, cpf, email, senha, Saldo], (err, results) => {
           if (err) {
             console.error('Erro ao inserir usuário:', err);
-            res.send('erro');
           } else {
-            res.send('sucess');
+            console.error('Sucesso');
           }
         });
       } else {
